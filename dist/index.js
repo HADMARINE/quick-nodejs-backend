263298e9530a9d945efea4e954e8726b6a28c878
HADMARINE <joonlee031@live.com>
2019-11-05 20:10:03 +0900

    init commit

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+app.listen(4000, () => {
+    console.log("Listening on port 4000");
+});
+//# sourceMappingURL=index.js.map
