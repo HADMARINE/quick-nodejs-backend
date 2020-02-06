ef0ea609072113b1035db41898d542868e2c98c6
HADMARINE <tsx@kakao.com>
2020-02-06 15:46:45 +0900

    update: if server is not running beacuse of the port usage, make to retry with another port

--- index.ts
+++ index.ts
@@ -1,21 +1,45 @@
+/** @format */
+
 import http from 'http';
+import * as express from 'express';
+import chalk from 'chalk';
 const app = require('./app');
 const connectDB = require('./src/lib/connectDB');
 require('dotenv').config();
 
-import * as express from 'express';
 const router = express.Router();
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 4000;
+const PORT = parseInt(process.env.PORT || '4000');
 
-connectDB()
-  .then(() => {
-    server.listen(PORT, () => {
-      console.log(`App started on port ${PORT}`);
-    });
-  })
-  .catch((e: any) => console.error(e));
+function listen(port: number = PORT) {
+  server.listen(port);
+  let isError: boolean = false;
+  server.once('error', (err: any) => {
+    if (err.code === 'EADDRINUSE') {
+      console.log(
+        chalk.yellow(
+          `Port ${port} is currently in use. Retrying with port ${port + 1}`
+        )
+      );
+      let newPort = port + 1;
+      listen(newPort);
+      isError = true;
+    }
+  });
+  server.once('listening', () => {
+    if (!isError) {
+      console.log(
+        chalk.black.bgGreen(`App started on port`) +
+          chalk.green.bold(` ${port}`)
+      );
+    }
+  });
+}
+
+connectDB().then(() => {
+  listen();
+});
 
 module.exports = router;
