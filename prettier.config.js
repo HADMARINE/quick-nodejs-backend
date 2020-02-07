2346dd6d6a549d344cfd074ac6f3a000f3825aa0
HADMARINE <tsx@kakao.com>
2020-02-07 15:36:32 +0900

    Update prettier.config.js

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,8 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
   bracketSpacing: true,
-  arrowParens: 'always',
-  insertPragma: true
+  arrowParens: 'always'
 };
