0f9cba9b954297935effe0bad70030f9f14988ab
HADMARINE <contact@hadmarine.com>
2020-04-10 00:23:04 +0900

    Improve app.ts code quality

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import cors from 'cors';
 import helmet from 'helmet';
 
 import getRoutes from '@lib/startup/getRoutes';
-import Error from '@error';
 import checkInitializeProjectSettings from '@lib/startup/checkInitializeProjectSettings';
+import error from '@error';
+import { defaultMessage, defaultCode } from '@lib/httpCode';
+
+interface Error {
+  status?: number;
+  message?: string;
+  code?: string;
+  data?: Record<string, any>;
+}
 
 const app = express();
 
@@ -33,18 +41,17 @@ getRoutes().forEach((data) => {
 
 // 404
 app.use((req) => {
-  Error.PageNotFound(req.url);
+  error.PageNotFound(req.url);
 });
 
 // Error handler
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   const status = error.status || 500;
   const message =
-    error.message && error.expose
-      ? error.message
-      : 'Unknown Error has occured.';
-  const code = error.code || 'UNKNOWN_ERROR';
+    error.message && error.expose ? error.message : defaultMessage(status);
+  const code = error.code || defaultCode(status);
   const data = error.data || {};
+
   if (!error.expose || process.env.NODE_ENV === 'development') {
     console.error(error);
   }
