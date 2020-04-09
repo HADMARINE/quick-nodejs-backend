991ba414dbc3235ea3fc0a904c7a21ab13990ed0
HADMARINE <contact@hadmarine.com>
2020-04-09 17:17:00 +0900

    Add boilerplate codes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,9 +2,19 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.all('/', (req, res) => {
   const date: Date = new Date();
-  res.send(date);
+  res.send(
+    `TIMENOW:${date}:<br/>See api info on <a href="/info"><b>GET /info</b></a>`,
+  );
+});
+
+router.get('/info', (req, res) => {
+  const data = {
+    v0: 'deprecated',
+    v1: 'production',
+  };
+  res.send(data);
 });
 
 export default router;
