f4fa0e1ba2164a3fa8d2c818057dbab3442b0431
HADMARINE <contact@hadmarine.com>
2020-04-10 22:03:46 +0900

    Update mainpage

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from 'express';
+import welcome from '@src/pages/Welcome';
+import moment from 'moment';
 const router = Router();
 
 router.all('/', (req, res) => {
-  const date: Date = new Date();
   res.send(
-    `TIMENOW:${date}:<br/>See api info on <a href="/info"><b>GET /info</b></a>`,
+    welcome(
+      'TS-NODE-EXPRESS-MONGO BACKEND',
+      `TIMENOW: ${moment().format(
+        'YYYY-MM-DD HH:mm:ss',
+      )}<br/> See api info on <a href="/info"><b>GET /info</b></a>`,
+    ),
   );
+});
+
+router.all('/time', (req, res) => {
+  res.send(moment().format('YYYY-MM-DD_HH:mm:ss'));
 });
 
 router.get('/info', (req, res) => {
