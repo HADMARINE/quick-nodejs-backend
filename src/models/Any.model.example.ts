3e1587f8d5ce54ad266eda05978dd46f0efba32e
HADMARINE <contact@hadmarine.com>
2020-04-10 22:32:00 +0900

    Update Any Model

--- src/models/Any.model.example.ts
+++ src/models/Any.model.example.ts
@@ -4,14 +4,16 @@ export interface AnyInterface {
   // Add Schema here
 }
 
-export interface AnyDocument extends Document, AnyInterface {
-  // Add Methods here
-}
-
 const AnySchema: Schema = new Schema({
   // Configure Mongoose Schema here
 });
 
+export interface AnyDocument extends Document, AnyInterface {
+  // Add Methods here
+}
+
 // AnySchema.methods.~~
 
-export default model<AnyDocument>('Any', AnySchema);
+const AnyModel = model<AnyDocument>('Any', AnySchema);
+
+export default AnyModel;
