e5a43a961b709bfa3d0e92fd37b88557577faaf7
HADMARINE <contact@hadmarine.com>
2020-04-13 15:55:26 +0900

    Update Session Model

--- src/models/Session.ts
+++ src/models/Session.ts
@@ -1,28 +1,39 @@
 import mongoose, { model, Schema, Document, Model } from 'mongoose';
 import Authorization from '@util/Authorization';
 import error from '@error';
+import jwt from 'jsonwebtoken';
+import Assets from '@util/Assets';
 
 export interface SessionInterface {
-  token: string;
+  jwtid: string;
   user: Schema.Types.ObjectId;
+  expire: number;
 }
 
 const SessionSchema: Schema = new Schema({
-  token: { type: String, required: true },
+  jwtid: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
+  expire: { type: Number, required: true },
 });
 
 export interface SessionDocument extends Document, SessionInterface {
-  registerToken(token: string, _id: Schema.Types.ObjectId): Promise<void>;
+  registerToken(token: string): Promise<void>;
 }
 
 SessionSchema.methods.registerToken = async function (
   token: string,
-  _id: Schema.Types.ObjectId,
 ): Promise<void> {
   await Authorization.token.verify.manual(token, 'refresh', true);
   try {
-    await SessionModel.create({ token, user: _id });
+    const tokenValue: any = jwt.decode(token);
+    if (!tokenValue._id || !tokenValue.exp || !tokenValue.jwtid) {
+      throw error.authorization.tokeninvalid();
+    }
+    await SessionModel.create({
+      jwtid: tokenValue.jwtid,
+      user: tokenValue._id,
+      expire: tokenValue.exp,
+    });
   } catch (e) {
     throw error.db.create('Session');
   }
