c7eb78045fa917fb50ac27d9b071e9d53d543d73
HADMARINE <contact@hadmarine.com>
2020-04-13 17:24:33 +0900

    Moved auth.controller.ts to upper dir

--- src/routes/v1/auth.controller.ts
+++ src/routes/v1/auth.controller.ts
@@ -10,24 +10,12 @@ export default new (class extends Controller {
       this.assets.apiRateLimiter(1, 10),
       this.resignAccessToken,
     );
-    this.router.get(
-      '/test/admin',
-      this.assets.apiRateLimiter(1, 20),
-      this.authorization.authority.admin,
-      this.checkUserLogin,
-    );
-    this.router.get(
-      '/test/user',
-      this.assets.apiRateLimiter(1, 20),
-      this.authorization.authority.user,
-      this.checkUserLogin,
-    );
   }
 
   private signInUser = this.Wrapper(async (req, res) => {
     const startTime = Date.now();
     const { userid, password } = req.body;
-    this.assets.checkNull([userid, password]);
+    this.assets.checkNull(userid, password);
     const user: any = await User.findOne({ userid }).exec();
     if (!user) {
       await this.assets.delayExact(startTime);
@@ -48,7 +36,7 @@ export default new (class extends Controller {
 
   private resignAccessToken = this.Wrapper(async (req, res) => {
     const { token } = req.body;
-    this.assets.checkNull([token]);
+    this.assets.checkNull(token);
     const tokenValue = await this.authorization.token.verify.refresh(token);
     const renewToken = await this.authorization.token.create.manual(
       {
@@ -63,9 +51,5 @@ export default new (class extends Controller {
       { token: renewToken },
       { message: 'Token creation successful' },
     );
-  });
-
-  private checkUserLogin = this.Wrapper((req, res) => {
-    this.Response(res, 200);
   });
 })();
