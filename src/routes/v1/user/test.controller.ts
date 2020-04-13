bd7dfa67ef8068d3c92c25f11acfd06d6adf2dc8
HADMARINE <contact@hadmarine.com>
2020-04-13 17:24:54 +0900

    Create user authority test endpoint (/v1/test/admin|user)

---
+++ src/routes/v1/user/test.controller.ts
@@ -0,0 +1,25 @@
+import Controller from '@lib/blueprint/Controller';
+import User from '@models/User';
+
+export default new (class extends Controller {
+  constructor() {
+    super();
+
+    this.router.get(
+      '/admin',
+      this.assets.apiRateLimiter(1, 20),
+      this.authorization.authority.admin,
+      this.checkUserLogin,
+    );
+    this.router.get(
+      '/user',
+      this.assets.apiRateLimiter(1, 20),
+      this.authorization.authority.user,
+      this.checkUserLogin,
+    );
+  }
+
+  private checkUserLogin = this.Wrapper((req, res) => {
+    this.Response(res, 200);
+  });
+})();
