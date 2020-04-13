75ff5cf69ef1ab8addfe680300abdac0acdc6071
HADMARINE <contact@hadmarine.com>
2020-04-13 14:58:34 +0900

    issue fix : not creating password data properly on patch

--- src/routes/v1/user/index.controller.ts
+++ src/routes/v1/user/index.controller.ts
@@ -6,17 +6,38 @@ export default new (class extends Controller {
   constructor() {
     super();
     this.router.post('/', this.createUser);
+    this.router.patch('/', this.authorization.authority.user, this.updateUser);
   }
 
   private createUser = this.Wrapper(async (req, res) => {
     const { userid, password } = req.body;
     this.assets.checkNull([userid, password]);
-    const user = await User.create({ userid, password });
+    const hashResult = this.authorization.password.create(password);
+    const user = await User.create({
+      userid,
+      password: hashResult.password,
+      enckey: hashResult.enckey,
+    });
     this.Response(
       res,
       201,
       { user },
       { message: 'Created user successfully.' },
+    );
+  });
+
+  private updateUser = this.Wrapper(async (req, res) => {
+    const { password } = req.body;
+    this.assets.checkNull([password]);
+    const hashResult = this.authorization.password.create(password);
+    const user = await User.findByIdAndUpdate(req.body.userData._id, {
+      $set: { password: hashResult.password, enckey: hashResult.enckey },
+    }).exec();
+    this.Response(
+      res,
+      202,
+      { user },
+      { message: 'Update user data successful' },
     );
   });
 })();
