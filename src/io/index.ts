7551725bbfd54bf78499dde4ea2f0c5c564c997f
HADMARINE <contact@hadmarine.com>
2020-04-17 10:55:51 +0900

    Update io types

--- src/io/index.ts
+++ src/io/index.ts
@@ -1,9 +1,10 @@
 import socketio from 'socket.io';
+import http from 'http';
 
-export default function (server: any) {
+export default function (server: http.Server) {
   const io = socketio(server);
 
-  io.on('connection', (socket: any) => {
+  io.on('connection', (socket: socketio.Server) => {
     socket.on('disconnect', (data: any) => {});
   });
 }
