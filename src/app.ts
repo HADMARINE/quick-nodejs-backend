c0e8ab7579d7657a34a0882d61f1b6ef9d5fb393
HADMARINE <contact@hadmarine.com>
2020-04-17 10:55:36 +0900

    Functionize root executions

--- src/app.ts
+++ src/app.ts
@@ -16,64 +16,71 @@ import ipfilter from '@lib/middlewares/ipfilter';
 
 const app = express();
 
-// Check Initial Environment Settings
-checkInitializeProjectSettings();
+function App() {
+  // Check Initial Environment Settings
+  checkInitializeProjectSettings();
+  cron();
 
-// Security settings
-app.use(
-  cors({
-    origin:
-      process.env.NODE_ENV === 'development'
-        ? '*'
-        : process.env.REQUEST_URI || '*',
-  }),
-);
-app.use(helmet());
-app.use(Assets.apiRateLimiter());
-app.use(Assets.wrapper(ipfilter));
+  // Enable if you're behind a reverse proxy
+  // see https://expressjs.com/en/guide/behind-proxies.html
+  // app.set('trust proxy', 1);
 
-// Morgan Logger
-app.use(
-  morgan(
-    '[:date[iso]]: :method :url - :status(:total-time[3]ms) [:remote-addr :user-agent HTTP::http-version]',
-    {
-      stream: fs.createWriteStream(path.join(__dirname, 'tmp/access.log'), {
-        flags: 'a',
-      }),
-    },
-  ),
-);
+  // Morgan Logger
+  app.use(
+    morgan(
+      '[:date[iso]]: :method :url - :status(:total-time[3]ms) [:remote-addr :user-agent HTTP::http-version]',
+      {
+        stream: fs.createWriteStream(path.join(__dirname, 'tmp/access.log'), {
+          flags: 'a',
+        }),
+      },
+    ),
+  );
 
-// express-fileupload
-app.use(
-  fileUploader({
-    limits: { fileSize: 50 * 1024 * 1024 },
-    useTempFiles: true,
-    tempFileDir: '/tmp/file/',
-    debug: process.env.NODE_ENV === 'development' ? true : false,
-  }),
-);
+  // Security settings
+  app.use(
+    cors({
+      origin:
+        process.env.NODE_ENV === 'development'
+          ? '*'
+          : process.env.REQUEST_URI || '*',
+    }),
+  );
+  app.use(helmet());
+  app.use(Assets.apiRateLimiter());
+  app.use(Assets.wrapper(ipfilter));
 
-// Parser
-app.use(express.json({ limit: '25mb' }));
-app.use(express.urlencoded({ extended: true, limit: '25mb' }));
+  // express-fileupload
+  app.use(
+    fileUploader({
+      limits: { fileSize: 50 * 1024 * 1024 },
+      useTempFiles: true,
+      tempFileDir: '/tmp/file/',
+      debug: process.env.NODE_ENV === 'development' ? true : false,
+    }),
+  );
 
-// Public files
-app.use(express.static('public'));
+  // Public files
+  app.use(express.static('public'));
 
-// Auto Routing
-getRoutes().forEach((data) => {
-  app.use(data.path || '/', data.router);
-});
+  // Parser
+  app.use(express.json({ limit: '25mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 
-// 404
-app.use((req) => {
-  throw error.access.pagenotfound(req.url);
-});
+  // Auto Routing
+  getRoutes().forEach((data) => {
+    app.use(data.path || '/', data.router);
+  });
 
-// Error handler
-app.use(errorHandler);
+  // 404
+  app.use((req) => {
+    throw error.access.pagenotfound(req.url);
+  });
 
-cron();
+  // Error handler
+  app.use(errorHandler);
 
-export default app;
+  return app;
+}
+
+export default App();
--- src/index.ts
+++ src/index.ts
@@ -6,67 +6,69 @@ import io from '@src/io';
 import connectDB from '@lib/startup/connectDB';
 import logger from '@lib/logger';
 
-logger.info(
-  'Created project TSNODE-backend-template by HADMARINE(https://github.com/hadmarine)',
-);
+function Root() {
+  logger.info(
+    'Created project TSNODE-backend-template by HADMARINE(https://github.com/hadmarine)',
+  );
 
-const router = Router();
-const server = http.createServer(app);
+  const router = Router();
+  const server = http.createServer(app);
 
-// io(server); // Enable when using socket.io
+  // io(server); // Enable when using socket.io
 
-const PORT = parseInt(process.env.PORT || '4000', 10);
+  const PORT = parseInt(process.env.PORT || '4000', 10);
 
-function listen(port: number = PORT): void {
-  if (port <= 0 || port >= 65536) {
-    logger.error(`PORT Range is Invalid. Recieved port : ${port}`);
+  function listen(port: number = PORT): void {
+    if (port <= 0 || port >= 65536) {
+      logger.error(`PORT Range is Invalid. Recieved port : ${port}`);
 
-    if (process.env.PORT_STRICT === 'true') {
-      logger.error(
-        ' Set PORT_STRICT to false on your .env if you want to execute anyway.',
-      );
-      throw new Error('PORT STRICT');
+      if (process.env.PORT_STRICT === 'true') {
+        logger.error(
+          ' Set PORT_STRICT to false on your .env if you want to execute anyway.',
+        );
+        throw new Error('PORT STRICT');
+      }
+      port = 20000;
+      logger.info(`Retrying with Port ${port}`);
     }
-    port = 20000;
-    logger.info(`Retrying with Port ${port}`);
+    server.listen(port);
+
+    let isError = false;
+    server.once('error', (err: any) => {
+      if (process.env.PORT_STRICT === 'true') {
+        logger.error(` Port ${port} is already in use.`);
+        logger.info(
+          'Set PORT_STRICT to false on your .env if you want to execute anyway.',
+        );
+        throw new Error('PORT STRICT');
+      }
+      if (err.code === 'EADDRINUSE') {
+        logger.info(
+          `Port ${port} is currently in use. Retrying with port ${port + 1}`,
+        );
+        const newPort = port === 65535 ? 20000 : port + 1;
+        listen(newPort);
+        isError = true;
+      }
+    });
+    server.once('listening', () => {
+      if (!isError) {
+        logger.success(
+          chalk.black.bgGreen(` App started on port `) +
+            chalk.green.bold(` ${port}`),
+        );
+      }
+    });
   }
-  server.listen(port);
 
-  let isError = false;
-  server.once('error', (err: any) => {
-    if (process.env.PORT_STRICT === 'true') {
-      logger.error(` Port ${port} is already in use.`);
-      logger.info(
-        'Set PORT_STRICT to false on your .env if you want to execute anyway.',
-      );
-      throw new Error('PORT STRICT');
-    }
-    if (err.code === 'EADDRINUSE') {
-      logger.info(
-        `Port ${port} is currently in use. Retrying with port ${port + 1}`,
-      );
-      const newPort = port === 65535 ? 20000 : port + 1;
-      listen(newPort);
-      isError = true;
-    }
-  });
-  server.once('listening', () => {
-    if (!isError) {
-      logger.success(
-        chalk.black.bgGreen(` App started on port `) +
-          chalk.green.bold(` ${port}`),
-      );
-    }
+  process.env.NODE_ENV = process.env.NODE_ENV
+    ? process.env.NODE_ENV
+    : 'development';
+
+  // tslint:disable-next-line: no-floating-promises
+  connectDB().then(() => {
+    listen();
   });
 }
 
-process.env.NODE_ENV = process.env.NODE_ENV
-  ? process.env.NODE_ENV
-  : 'development';
-
-// tslint:disable-next-line: no-floating-promises
-connectDB().then(() => {
-  listen();
-});
-
-export default router;
+export default Root();
