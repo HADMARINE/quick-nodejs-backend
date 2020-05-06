f133da8558d703392b16eac79c9ce429ded5f931
HADMARINE <contact@hadmarine.com>
2020-05-06 17:18:25 +0900

    Remove unused comments

--- src/lib/middlewares/morgan.ts
+++ src/lib/middlewares/morgan.ts
@@ -6,7 +6,6 @@ export default function (): express.RequestHandler {
   if (!fs.existsSync('tmp')) {
     fs.mkdirSync('tmp');
   }
-  // fs.existsSync('../tmp') || fs.mkdirSync('../tmp');
   return morgan(
     '[:date[iso]]: :method :url - :status(:total-time[3]ms) [:remote-addr :user-agent HTTP::http-version]',
     {
