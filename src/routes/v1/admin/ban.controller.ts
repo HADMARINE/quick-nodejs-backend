c21a64642df459ee3e1979450924ec6d64d8dc9d
HADMARINE <contact@hadmarine.com>
2020-07-19 14:15:37 +0900

    Update routes

--- src/routes/v1/admin/ban.controller.ts
+++ src/routes/v1/admin/ban.controller.ts
@@ -1,38 +1,38 @@
-import Controller from '@lib/blueprint/Controller';
+import C from '@lib/blueprint/Controller';
 import Banip from '@models/Banip';
 
-export default new (class extends Controller {
+export default new (class extends C {
   constructor() {
     super();
-    this.router.get(`/ip`, this.auth.authority.admin, this.getBannedIpList);
-    this.router.get(`/ip/:ip`, this.auth.authority.admin, this.getBannedIp);
-    this.router.post('/ip', this.auth.authority.admin, this.banIp);
-    this.router.delete('/ip', this.auth.authority.admin, this.unbanIp);
+    this.router.get(`/ip`, C.auth.authority.admin, this.getBannedIpList);
+    this.router.get(`/ip/:ip`, C.auth.authority.admin, this.getBannedIp);
+    this.router.post('/ip', C.auth.authority.admin, this.banIp);
+    this.router.delete('/ip', C.auth.authority.admin, this.unbanIp);
   }
 
-  private getBannedIpList = this.Wrapper(async (req, res) => {
+  private getBannedIpList = C.Wrapper(async (req, res) => {
     const { skip, limit } = req.query;
     const banip = await Banip.find()
       .select('ip reason due')
-      .skip(parseInt(this.assets.data.filter(skip, 'string') || 0, 10))
-      .limit(parseInt(this.assets.data.filter(limit, 'string') || 10, 10))
+      .skip(parseInt(C.assets.data.filter(skip, 'string') || 0, 10))
+      .limit(parseInt(C.assets.data.filter(limit, 'string') || 10, 10))
       .sort('-id')
       .exec();
-    if (!banip) throw this.error.db.notfound();
+    if (!banip) throw C.error.db.notfound();
     res(200, { ...banip });
   });
 
-  private getBannedIp = this.Wrapper(async (req, res) => {
+  private getBannedIp = C.Wrapper(async (req, res) => {
     const { ip } = req.params;
     const banip = await Banip.findOne({ ip }).select('ip reason due').exec();
-    if (!banip) throw this.error.db.notfound();
+    if (!banip) throw C.error.db.notfound();
     res(200, banip);
   });
 
-  private banIp = this.Wrapper(async (req, res) => {
+  private banIp = C.Wrapper(async (req, res) => {
     let { reason, due } = req.body;
     const { userData, ip } = req.body;
-    this.assets.checkNull(ip);
+    C.assets.checkNull(ip);
     reason = reason
       ? `${reason} - ISSUED BY ADMIN(${userData.userid})`
       : `ISSUED BY ADMIN(${userData.userid})`;
@@ -41,14 +41,14 @@ export default new (class extends Controller {
     res(201, { banip }, { message: 'Successfully banned ip' });
   });
 
-  private unbanIp = this.Wrapper(async (req, res) => {
+  private unbanIp = C.Wrapper(async (req, res) => {
     let { ip } = req.body;
-    this.assets.checkNull(ip);
-    ip = this.assets.returnArray(ip);
+    C.assets.checkNull(ip);
+    ip = C.assets.returnArray(ip);
     const banip = await Banip.deleteMany({
       ip: { $in: ip },
     }).exec();
-    if (!banip.n) throw this.error.db.notfound();
+    if (!banip.n) throw C.error.db.notfound();
     if (ip.length > banip.n) {
       res(200, undefined, {
         message: 'Update successful, but could not found some datas.',
