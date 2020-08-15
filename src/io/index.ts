a5829375393197aa260e0d8cfb0c622efac6bd0c
HADMARINE <contact@hadmarine.com>
2020-08-15 15:50:54 +0900

    Update io index

--- src/io/index.ts
+++ src/io/index.ts
@@ -1,22 +1,41 @@
 import socketio from 'socket.io';
 import http from 'http';
+import getEvents from '@lib/startup/getSocketEvents';
+import logger from '@lib/logger';
 
 export default function (server: http.Server) {
   const io = socketio(server, {
-    origins: `${process.env.REQUEST_URI}`,
-  });
-  io.on('connection', (socket: socketio.Server) => {
-    actions(socket);
-  });
-}
-
-function actions(socket: socketio.Server) {
-  socket.on('hello', (data: any) => {
-    socket.emit('world', 'WORLD!');
-    return;
+    origins:
+      process.env.REQUEST_URI === '*'
+        ? undefined
+        : `${process.env.REQUEST_URI}`,
+    path: '/socket',
   });
 
-  socket.on('disconnect', (data: any) => {
-    console.log('disconnected');
+  const eventList = getEvents();
+
+  io.on('connection', (socket: Socketio.Socket) => {
+    Object.keys(eventList.controlled).forEach((key) => {
+      socket.on(key, (...data: any[]) => {
+        const guidedSocketClass = eventList.controlled[key];
+        try {
+          new guidedSocketClass(io, socket, data).main();
+        } catch (e) {
+          logger.warn(
+            `Controlled socket class [${key}] has error, ignoring...`,
+          );
+          logger.debug(e);
+        }
+      });
+    });
+    Object.keys(eventList.raw).forEach((key) => {
+      const rawSocketFunction = eventList.raw[key];
+      try {
+        rawSocketFunction(io, socket);
+      } catch (e) {
+        logger.warn(`Raw socket function [${key}] has error, ignoring...`);
+        logger.debug(e);
+      }
+    });
   });
 }
