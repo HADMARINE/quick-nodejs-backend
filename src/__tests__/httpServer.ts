29e3ead7694245185463bf2091fc56490c28a72f
HADMARINE <tsx@kakao.com>
2021-02-04 12:08:07 +0900

    Update test codes : use rarely-used port instead 80

---
+++ src/__tests__/httpServer.ts
@@ -0,0 +1,14 @@
+import supertest from 'supertest';
+import Test from '@util/Test';
+
+describe('Test V1 Router', () => {
+  it('should return result true', async () => {
+    try{
+      const res = await supertest.agent((await Test.server.create())).get('/v1');
+      await Test.server.close();
+      expect(res.body.result).toBe(true);
+    } catch(e) {
+      throw e;
+    }
+  });
+});
